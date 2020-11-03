176cae8bf8f8f988bcf3fe695221a5919b12e723
Alaeddinehnana <alaeddine.hnana@esprit.tn>
2020-11-03 14:59:21 +0100

    Junit Test Part1

---
+++ src/test/java/tn/esprit/spring/TimesheetOneServiceImplTest.java
@@ -0,0 +1,48 @@
+package tn.esprit.spring;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.repository.MissionRepository;
+import tn.esprit.spring.services.IEntrepriseService;
+import tn.esprit.spring.services.ITimesheetOneService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimesheetOneServiceImplTest {
+
+	@Autowired
+	ITimesheetOneService timesheetService;
+	@Autowired
+	MissionRepository missionRepo;
+	@Autowired
+	IEntrepriseService entrepriseService;
+	
+	@Test
+	public void testAjoutMission() {
+		Mission m = new Mission("mission1","mission de développement à France");
+		int missionId = timesheetService.ajouterMission(m);
+		assertNotNull(missionId);
+	}
+	
+	@Test
+	public void testAffecterMissionADepartement() {
+		Departement dep = new Departement("Informatique");
+		int depId = entrepriseService.ajouterDepartement(dep);
+		timesheetService.affecterMissionADepartement(1, depId);
+		assertEquals(missionRepo.findById(1).get().getDepartement().getId(),depId);
+	}
+	
+	
+	
+	
+}
