83ccb82567b54ece6bb0797e6c9ca0374fae272a
oussemasaadouli <oussema.saadouli@esprit.tn>
2020-11-04 13:06:13 +0100

    deleting unused ports

--- src/main/java/tn/esprit/spring/repository/ContratRepository.java
+++ src/main/java/tn/esprit/spring/repository/ContratRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.Contrat;
-import tn.esprit.spring.entities.Mission;
-import tn.esprit.spring.entities.Timesheet;
+
 
 @Repository
 public interface ContratRepository extends CrudRepository<Contrat, Integer>{
